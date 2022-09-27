913469a6f28fa7cc80befb3b79660df60998d7a6
Hannes Van den Berghe <hannes.vandenberghe@icapps.com>
2022-09-27 15:38:27 +0200

    Initial Commit

--- unsplash/Modules/Welcome/WelcomeViewController.swift
+++ unsplash/Modules/Welcome/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  unsplash
 //
 //  Created by Hannes Van den Berghe on 27/09/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class WelcomeViewController: UIViewController {
+    
+    private let viewModel: WelcomeViewModel = WelcomeViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ unsplash/Modules/Welcome/WelcomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  WelcomeViewModel.swift
+//  unsplash
+//
+//  Created by Hannes Van den Berghe on 27/09/2022.
+//
+
+import Foundation
+
+struct WelcomeViewModel {
+    
+}
